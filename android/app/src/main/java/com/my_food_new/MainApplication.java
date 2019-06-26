f2e8760aa4140192ce3d0c9df2698e84b28a4bd2
Sơn Ngọc <sondnpt00343@gmail.com>
2019-06-26 20:48:09 +0700

    Upgrade all dependency

--- android/app/src/main/java/com/my_food_new/MainApplication.java
+++ android/app/src/main/java/com/my_food_new/MainApplication.java
@@ -3,6 +3,7 @@ package com.my_food_new;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.geektime.rnonesignalandroid.ReactNativeOneSignalPackage;
 import com.imagepicker.ImagePickerPackage;
 import com.reactnativecommunity.asyncstorage.AsyncStoragePackage;
 import com.horcrux.svg.SvgPackage;
@@ -32,6 +33,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new ReactNativeOneSignalPackage(),
             new ImagePickerPackage(),
             new AsyncStoragePackage(),
             new SvgPackage(),
