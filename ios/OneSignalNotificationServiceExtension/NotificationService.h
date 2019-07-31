658ccb3069a78cd67c89f1da11f3a94aa63e3a48
thuc le huy <thuc.lehuy@gmail.com>
2019-07-31 18:22:37 +0700

    Cau hinh push iOS

---
+++ ios/OneSignalNotificationServiceExtension/NotificationService.h
@@ -0,0 +1,13 @@
+//
+//  NotificationService.h
+//  OneSignalNotificationServiceExtension
+//
+//  Created by Le Huy Thuc on 7/31/19.
+//  Copyright © 2019 Facebook. All rights reserved.
+//
+
+#import <UserNotifications/UserNotifications.h>
+
+@interface NotificationService : UNNotificationServiceExtension
+
+@end
