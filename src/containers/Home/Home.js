68d1462bbf0652ed43808c9095167b9cf469da03
Sơn Ngọc <sondnpt00343@gmail.com>
2019-10-28 09:19:25 +0000

    Add rating

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -41,6 +41,12 @@ class Home extends Component {
     try {
       const response = await APIHandler.user_site_home();
       if (response && response.status == STATUS_SUCCESS) {
+        if (response.data.vote_cart && response.data.vote_cart.site_id) {
+          Actions.rating({
+            cart_data: response.data.vote_cart
+          });
+        }
+
         this.setState({
           site: response.data.site,
           sites: response.data.sites,
