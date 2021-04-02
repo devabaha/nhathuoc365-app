c91da018308e28c11602f0a979d6e9224c8fb37b
ABAHA <app.abaha.net@gmail.com>
2021-04-02 15:25:46 +0700

    no message

---
+++ src/components/Home/component/PrimaryActions/SubActionButton/SubActionButton.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import {TouchableOpacity, View, Text, StyleSheet} from 'react-native';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import appConfig from 'app-config';
+
+const styles = StyleSheet.create({
+  add_store_action_btn: {
+    paddingVertical: 4,
+    paddingHorizontal: 12,
+  },
+  add_store_action_btn_box: {
+    alignItems: 'center',
+  },
+  add_store_action_label: {
+    fontSize: 14,
+    color: '#404040',
+    marginTop: 4,
+    textAlign: 'center',
+  },
+  icon: {
+    fontSize: 28,
+    color: appConfig.colors.primary,
+  },
+});
+
+const SubActionButton = ({
+    iconName,
+    label,
+    
+    wrapperStyle = {},
+    containerStyle  ={},
+    iconStyle = {},
+    labelStyle = {},
+
+    onPress = () => {},
+}) => {
+  return (
+    <TouchableOpacity
+      onPress={onPress}
+      style={[styles.add_store_action_btn, wrapperStyle]}>
+      <View style={[styles.add_store_action_btn_box, containerStyle]}>
+        <Ionicons name={iconName} style={[styles.icon, iconStyle]} />
+        {!!label && (
+          <Text style={[styles.add_store_action_label, labelStyle]}>
+            {label}
+          </Text>
+        )}
+      </View>
+    </TouchableOpacity>
+  );
+};
+
+export default React.memo(SubActionButton);
