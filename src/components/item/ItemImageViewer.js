a53b41fa7d4ad57a68647b554ccef982d9284d07
kienluu612 <kienluu612@gmail.com>
2021-07-12 09:11:34 +0700

    add options button.

--- src/components/item/ItemImageViewer.js
+++ src/components/item/ItemImageViewer.js
@@ -1,15 +1,19 @@
 /* @flow */
 
 import React, {Component} from 'react';
-import {View, StyleSheet, TouchableHighlight} from 'react-native';
+import {View, StyleSheet, TouchableHighlight, TouchableOpacity} from 'react-native';
 
 //library
 import Icon from 'react-native-vector-icons/FontAwesome';
+import AntDesignIcon from 'react-native-vector-icons/AntDesign';
+import EntypoIcon from 'react-native-vector-icons/Entypo';
 import {Actions} from 'react-native-router-flux';
 import ImageViewer from 'react-native-image-zoom-viewer';
 import EventTracker from '../../helper/EventTracker';
 import {saveImage} from '../../helper/image';
 import ActionSheet from 'react-native-actionsheet';
+import appConfig from 'app-config';
+import { HIT_SLOP } from 'app-packages/tickid-chat/constants';
 
 class ItemImageViewer extends Component {
   static defaultProps = {
@@ -55,7 +59,28 @@ class ItemImageViewer extends Component {
       saveImage(this.props.images[this.imageIndex].url);
     }
   };
+  renderHeader = () => {
+    return (
+      <View
+        style={{
+          top: appConfig.device.statusBarHeight,
+          flexDirection: 'row',
+          position:'absolute',
+          zIndex:1
 
+        }}>
+        <TouchableOpacity hitSlop={HIT_SLOP} style={{ padding: 12, flex: 1}}>
+          <EntypoIcon size={20} name="chevron-thin-left" color="#fff"/>
+        </TouchableOpacity>
+        <TouchableOpacity style={{ padding: 12}}>
+          <AntDesignIcon size={20} name="download" color="#fff"/>
+        </TouchableOpacity>
+        <View style={{ padding: 12}}>
+          <EntypoIcon size={20} name="dots-three-vertical" color="#fff"/>
+        </View>
+      </View>
+    );
+  };
   render() {
     var {images} = this.props;
 
@@ -70,6 +95,7 @@ class ItemImageViewer extends Component {
           imageUrls={images}
           index={this.props.index}
           onChange={this.onChangeImageIndex}
+          renderHeader={this.renderHeader}
         />
 
         <TouchableHighlight
