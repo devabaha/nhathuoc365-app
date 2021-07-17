c5ab6c215e6a1b3341f49cad9e6e1c041b0ad01d
Lam Hai Au <lamah.dev@gmail.com>
2021-07-17 11:45:30 +0700

    no message

--- src/lib/Constant.js
+++ src/lib/Constant.js
@@ -2,6 +2,7 @@
 
 import { Platform, Dimensions, PixelRatio } from 'react-native';
 
+// API URL
 // global.MY_FOOD_API = 'https://abaha.net/';
 // global.MY_FOOD_API = 'http://localhost:8000/';
 global.MY_FOOD_API = 'http://apiapp.tickid.vn/';
