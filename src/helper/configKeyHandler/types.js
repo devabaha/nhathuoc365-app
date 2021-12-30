0a5303c5efff6fda7c67f33da41aea410bcbf29f
Minh Nguyen <minhnguyenit14@gmail.com>
2021-12-30 10:12:14 +0700

    finish

--- src/helper/configKeyHandler/types.js
+++ src/helper/configKeyHandler/types.js
@@ -52,4 +52,6 @@ export const CONFIG_KEY = {
   HIDE_VOUCHERS_ACCOUNT_KEY: 'hide_vouchers_account_key',
 
   HIDE_AFFILIATE_ACCOUNT_KEY: 'hide_affiliate_account_key',
+
+  PARTNER_REGISTRATION_LINK_KEY: 'partner_registration_link_key',
 };
