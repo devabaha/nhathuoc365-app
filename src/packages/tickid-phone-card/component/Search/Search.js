9a7f86e4a8d8bcc6f4b8fe30ca9c5239b7d7d70c
Kien Luu <kienluu612@gmail.com>
2022-01-15 06:29:40 +0000

    Restructure/src packages tickid phone card container phone card contact container kien

--- src/packages/tickid-phone-card/component/Search/Search.js
+++ src/packages/tickid-phone-card/component/Search/Search.js
@@ -1,71 +1,82 @@
-import React, { PureComponent } from 'react';
+import React, {PureComponent} from 'react';
 import PropTypes from 'prop-types';
-import {
-  View,
-  Image,
-  TextInput,
-  StyleSheet,
-  TouchableOpacity
-} from 'react-native';
-import searchImage from '../../assets/images/search.png';
-import closeImage from '../../assets/images/close.png';
-
+import {StyleSheet} from 'react-native';
+// helpers
+import {getTheme} from 'src/Themes/Theme.context';
+// context
+import {ThemeContext} from 'src/Themes/Theme.context';
+// constants
+import {CardBorderRadiusType, TypographyType} from 'src/components/base';
+// images
+import searchImage from 'src/assets/images/search.png';
+import closeImage from 'src/assets/images/close.png';
+// custom components
+import {Card, ImageButton, Input} from 'src/components/base';
+import Image from 'src/components/Image';
 class Search extends PureComponent {
+  static contextType = ThemeContext;
+
   static propTypes = {
     value: PropTypes.string,
     onChangeText: PropTypes.func,
     onClearText: PropTypes.func,
-    placeholder: PropTypes.string
+    placeholder: PropTypes.string,
   };
 
   static defaultProps = {
     value: '',
     onChangeText: () => {},
     onClearText: () => {},
-    placeholder: 'Tìm theo tên hoặc số điện thoại'
   };
 
+  get theme() {
+    return getTheme(this);
+  }
+
+  get placeholder() {
+    return this.props.placeholder || this.props.t('searchContactsPlaceholder');
+  }
   render() {
     return (
-      <View style={styles.container}>
+      <Card
+        row
+        borderRadiusSize={CardBorderRadiusType.SMALL}
+        style={styles.container}>
         <Image style={styles.icon} source={searchImage} />
-        <TextInput
+        <Input
+          type={TypographyType.LABEL_SEMI_LARGE}
           style={styles.textInput}
           placeholder={this.props.placeholder}
-          placeholderTextColor="#c7c7cd"
+          placeholderTextColor={this.theme.color.placeholder}
           value={this.props.value}
           onChangeText={this.props.onChangeText}
         />
         {!!this.props.value && (
-          <TouchableOpacity onPress={this.props.onClearText}>
-            <Image style={styles.icon} source={closeImage} />
-          </TouchableOpacity>
+          <ImageButton
+            imageStyle={styles.icon}
+            source={closeImage}
+            onPress={this.props.onClearText}
+          />
         )}
-      </View>
+      </Card>
     );
   }
 }
 
 const styles = StyleSheet.create({
   container: {
-    flexDirection: 'row',
-    alignItems: 'center',
     margin: 8,
-    backgroundColor: '#fff',
-    borderRadius: 4,
-    paddingHorizontal: 8
+    paddingHorizontal: 8,
   },
   icon: {
     width: 18,
-    height: 18
+    height: 18,
   },
   textInput: {
     flex: 1,
-    fontSize: 15,
-    color: '#333',
     paddingVertical: 12,
-    marginLeft: 8
-  }
+    marginLeft: 8,
+  },
 });
 
-export default Search;
+export default withTranslation('phoneCardContact')(Search);
